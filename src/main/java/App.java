95d397dce3775b25eeefd6ae30d645d701b0978e
Sky-Net <nadkrauler@gmail.com>
2022-11-11 22:05:59 +0400

    HelloWorld

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,20 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat one = (Cat)applicationContext.getBean("cat");
+        System.out.println(one.getCat());
+        Cat two = (Cat)applicationContext.getBean("cat");
+        System.out.println(two.getCat());
+
+        System.out.println(bean.equals(bean2));
+        System.out.println(one.equals(two));
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+
+    private String cat;
+
+    public Cat(String cat) {
+        this.cat = cat;
+    }
+
+    public String getCat() {
+        return cat;
+    }
+}
